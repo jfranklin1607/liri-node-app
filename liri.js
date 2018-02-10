9169cce34f78c48ca0b28750a04e39a7fd099281
jfranklin1607 <jfranklin1607@gmail.com>
2018-02-09 22:02:38 -0500

    liri

--- liri.js
+++ liri.js
@@ -22,28 +22,27 @@ request(queryUrl, function(error, response, body) {
         console.log('Actors: ' + JSON.parse(body).Actors);
     } else {
         console.log("If you haven't watched \"Mr. Nobody\", then you should: http://www.imdb.com/title/tt0485947/" + "\nIt's on Netflix!\n");
-    };
+    }
 
 //SPOTIFY REQUEST FOR CALLBACK
 
-var spotify = require('spotify-search');
+var Spotify = require('node-spotify-api');
+ 
+var spotify = new Spotify({
+  id: "397dacf738ec41fb9bb1b906b94f5781",
+  secret: "69a7d711f37246f4bd66a527e203349c"
+});
+ 
+search: function( type: artist OR album OR track, query: My search query, limit: 1 , callback) {
 
+  if (err) {
+    return console.log('Error occurred: ' + "The Sign, \nby Ace of Base\n");
+  }
+ 
+console.log(Spotify); 
 
-var songName = process.argv[2];
-
-var queryUrl = 'https://beta.developer.spotify.com/dashboard/applications' + songName + '&y=&plot=short&apikey=397dacf738ec41fb9bb1b906b94f5781';
-
-request(queryUrl, function(error, response, body) {
-
-    if (!error && response.statusCode === 200) {
-        console.log('Artist: ' + JSON.parse(body).Artist);
-        console.log('Song: ' + JSON.parse(body).Song);
-        console.log('Preview: ' + JSON.parse(body).Preview);
-        console.log('Album: ' + JSON.parse(body).Album);
-    } else {
-        console.log("The Sign, \nby Ace of Base\n");
-    }
-})
+    
+}
 
 });
 
