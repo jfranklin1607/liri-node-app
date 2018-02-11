52f74beb5a98e809d14a601017ea9d7b5fee87e9
jfranklin1607 <jfranklin1607@gmail.com>
2018-02-11 13:39:39 -0500

    liri

--- liri.js
+++ liri.js
@@ -1,48 +1,135 @@
+//Node modules imports needed to run the functions
+var fs = require("fs");
+var request = require("require");
+var keys = require("./keys.js");
+var twitter = require("twitter");
+var spotify = require("spotify");
+var appCommand = process.argv[2];
 
-//REQUEST FOR MOVIE CALLBACK//
+// Switch statement for appCommand
 
-var request = require('request');
-var util = require('util');
+switch(appCommand) {
+	case "my-tweets": myTweets(); break;
+	case "spotify-this-song": spotifyThisSong(); break;
+	case "movie-this": movieThis(); break;
+	case "do-what-it-says": doWhatItSays(); break;
 
-var movieName = process.argv[2];
+	//Default output in command line
 
-var queryUrl = 'http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&apikey=trilogy';
+	default: console.log("\n" + "Try typing one of the following commands after 'node liri.js' : " +"\n" +
+			"1. my-tweets 'any twitter name' "+"\n"+
+			"2. spotify-this-song 'any song name' "+"\n"+
+			"3. movie-this 'any movie name' "+"\n"+
+			"4. do-what-it-says."+"\n"+
+			"Please put the movie or song name in quotation marks if it's more than one word.");
+};
 
-request(queryUrl, function(error, response, body) {
+//Function for the movie request API
+function movieThis(){
+	var movie = process.argv[3];
+	if(!movie){
+		movie = "mr nobody";
+	}
+	params = movie
+	request("http://www.omdbapi.com/?t=" + params + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var movieObject = JSON.parse(body);
 
-    console.log(util.inspect(JSON.parse(body)));
+			var movieResults =
+			"Title: " +movieObject.Title+"\n"+
+			"Year: " +movieObject.Year+"\n"+ 
+			"Imdb Rating: " +movieObject.imdbRating+"\n"+ 
+			"Country: " +movieObject.Country+ "\n"+
+			"Language: " +movieObject.Language+"\n"+ 
+			"Plot: " +movieObject.Plot+"\n"+ 
+			"Actors: " +movieObject.Actors+"\n"+
+			"Rotten Tomatoes Rating: " + movieObject.tomatoRating+"\n"+
+			"Rotten TomatoesURL: " +movieObject.tomatoURL+"\n"
 
-    if (!error && response.statusCode === 200) {
-        console.log('Title: ' + JSON.parse(body).Title);
-        console.log('Release Year: ' + JSON.parse(body).Year);
-        console.log('Rating: ' + JSON.parse(body).Rating);
-        console.log('Rotten Tomatoes: ' + JSON.parse(body).RottenTomatoes);
-        console.log('Country: ' + JSON.parse(body).Country);
-        console.log('Plot: ' + JSON.parse(body).Plot);
-        console.log('Actors: ' + JSON.parse(body).Actors);
-    } else {
-        console.log("If you haven't watched \"Mr. Nobody\", then you should: http://www.imdb.com/title/tt0485947/" + "\nIt's on Netflix!\n");
-    }
+			console.log(movieResults);
+			log(movieResults);
+		} else{
+			console.log("Error :" + error);
+			return;
+		}
+ });
 
-//SPOTIFY REQUEST FOR CALLBACK
-
-var Spotify = require('node-spotify-api');
- 
-var spotify = new Spotify({
-  id: "397dacf738ec41fb9bb1b906b94f5781",
-  secret: "69a7d711f37246f4bd66a527e203349c"
-});
- 
-search: function( type: artist OR album OR track, query: My search query, limit: 1 , callback) {
-
-  if (err) {
-    return console.log('Error occurred: ' + "The Sign, \nby Ace of Base\n");
-  }
- 
-console.log(Spotify); 
-
-    
-}
-
-});
-
+};
+// Tweet function, uses the Twitter module to call the Twitter api
+	function myTweets() {
+		var client = new twitter({
+			consumer_key: keys.twitterKeys.consumer_key,
+			consumer_secret: keys.twitterKeys.consumer_secret,
+			access_token_key: keys.twitterKeys.access_token_key,
+			access_token_secret: keys.twitterKeys.access_token_secret, 
+		});
+		var twitterUsername = process.argv[3];
+		if(!twitterUsername){
+			twitterUsername = "Melvin Gordon";
+		}
+		params = {screen_name: twitterUsername};
+		client.get("statuses/user_timeline/", params, function(error, data, response){
+			if (!error) {
+				for(var i = 0; i < data.length; i++) {
+					//console.log(response); // Show the full response in the terminal
+					var twitterResults = 
+					"@" + data[i].user.screen_name + ": " + 
+					data[i].text + "\r\n" + 
+					data[i].created_at + "\r\n" + 
+					"------------------------------ " + i + " ------------------------------" + "\r\n";
+					console.log(twitterResults);
+					log(twitterResults); // calling log function
+				}
+			}  else {
+				console.log("Error :"+ error);
+				return;
+			}
+		});
+	}
+	// Spotify function, uses the Spotify module to call the Spotify api
+	function spotifyThisSong(songName) {
+		var songName = process.argv[3];
+		if(!songName){
+			songName = "What's my age again";
+		}
+		params = songName;
+		spotify.search({ type: "track", query: params }, function(err, data) {
+			if(!err){
+				var songInfo = data.tracks.items;
+				for (var i = 0; i < 5; i++) {
+					if (songInfo[i] != undefined) {
+						var spotifyResults =
+						"Artist: " + songInfo[i].artists[0].name + "\r\n" +
+						"Song: " + songInfo[i].name + "\r\n" +
+						"Album the song is from: " + songInfo[i].album.name + "\r\n" +
+						"Preview Url: " + songInfo[i].preview_url + "\r\n" + 
+						"------------------------------ " + i + " ------------------------------" + "\r\n";
+						console.log(spotifyResults);
+						log(spotifyResults); // calling log function
+					}
+				}
+			}	else {
+				console.log("Error :"+ err);
+				return;
+			}
+		});
+	};
+	// Do What It Says function, uses the reads and writes module to access the random.txt file and do what's written in it
+	function doWhatItSays() {
+		fs.readFile("random.txt", "utf8", function(error, data){
+			if (!error) {
+				doWhatItSaysResults = data.split(",");
+				spotifyThisSong(doWhatItSaysResults[0], doWhatItSaysResults[1]);
+			} else {
+				console.log("Error occurred" + error);
+			}
+		});
+	};
+	// Do What It Says function, uses the reads and writes module to access the log.txt file and write everything that returns in terminal in the log.txt file
+	function log(logResults) {
+	  fs.appendFile("log.txt", logResults, (error) = {
+	    if(error) {
+	      throw error;
+	    }
+	  });
+	}
